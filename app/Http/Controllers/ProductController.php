2300234a8b6c60ca98ffe92c3c7fcc5ee3c04766
rohmatsyam <rohmatsyam@yahoo.com>
2022-06-08 16:49:10 +0700

    "product delete success"

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,12 +13,18 @@ class ProductController extends Controller
         $req = $req->all();        
         $c = new LazopClient($this->url,env('LAZADA_KEY'), env('LAZADA_SECRET'));
         $request = new LazopRequest('/products/get','GET');
+        $request->addApiParam('filter','all');
         $response = $c->execute($request,$req['access_token']);
-    
-        $hasil = json_decode($response);
+        $hasil = json_decode($response);        
         if($hasil->code == "0"){
-            return view('products',['products'=>$hasil->data->products]);
-        }        
+            if($hasil->data->products){
+                return view('products',['products'=>$hasil->data->products]);
+            }else{
+                return redirect()->route('lazadahome');
+            }
+        }else{
+            dd($hasil);
+        }
     }
     public function deactivateProduct(Request $req){
         $req = $req->all();
@@ -29,8 +35,26 @@ class ProductController extends Controller
 
         $hasil = $c->execute($request, $req['access_token']);
         $hasil = json_decode($hasil);
-        if($hasil->data->success){            
-            return back()->with("success","Item with id ".$item_id." deactivated successfully!");
+        if($hasil->code == "0"){
+            return redirect()->route('producthome')->with("success","Item with id ".$item_id." deactivated successfully!");
+        }else{
+            return redirect()->route('producthome')->with("error",$hasil->message);            
+        }
+    }
+    public function deleteProduct(Request $req){
+        $req = $req->all();
+        $item_id = $req['item_id'];        
+        $sku_id = $req['sku_id'];        
+
+        $c = new LazopClient($this->url,env('LAZADA_KEY'), env('LAZADA_SECRET'));
+        $request = new LazopRequest('/product/remove');             
+        $request->addApiParam('seller_sku_list',"[\"SkuId_{$item_id}_{$sku_id}\"]");        
+        $hasil = $c->execute($request, $req['access_token']);
+        $hasil = json_decode($hasil);
+        if($hasil->code == "0"){
+            return redirect()->route('producthome')->with("success","Item with id ".$item_id." deleted successfully!");                        
+        }else{
+            return redirect()->route('producthome')->with("error",$hasil->message);
         }
     }
 }
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -14,6 +14,12 @@
                     <strong>{{ $message }}</strong>
                 </div>
             @endif
+            @if ($message = Session::get('error'))
+                <div class="alert alert-danger alert-block">
+                    <button type="button" class="btn-close" data-dismiss="alert">x</button>
+                    <strong>{{ $message }}</strong>
+                </div>
+            @endif
             <div>
                 <h2 class="fw-bolder text-center">All Products</h2>
                 <div class="container">
@@ -27,6 +33,7 @@
                                                 <th>No</th>
                                                 <th>ID</th>
                                                 <th>Category</th>
+                                                <th>SKU ID</th>
                                                 <th>Nama</th>
                                                 <th>Brand</th>
                                                 <th>Jumlah</th>
@@ -48,6 +55,7 @@
                                                     </th>
                                                     <td>{{ $product->item_id }}</td>
                                                     <td>{{ $product->primary_category }}</td>
+                                                    <td>{{ $product->skus[0]->SkuId }}</td>
                                                     <td>{{ $product->attributes->name }}</td>
                                                     <td>{{ $product->attributes->brand }}</td>
                                                     <td>{{ $product->skus[0]->quantity }}</td>
@@ -59,8 +67,8 @@
                                                     <td>{{ $product->status }}</td>
                                                     <td>
                                                         {{-- deactivate --}}
-                                                        <form action="{{ route('deactivateproduct') }}"
-                                                            method="POST">
+                                                        <form action="{{ route('deactivateproduct') }}" method="POST"
+                                                            class="form-group">
                                                             @csrf
                                                             <input name="item_id" type="hidden"
                                                                 value="{{ $product->item_id }}">
@@ -75,6 +83,22 @@
                                                     </td>
                                                     <td>
                                                         {{-- delete --}}
+                                                        <form action="{{ route('deleteproduct') }}" method="POST"
+                                                            class="form-group">
+                                                            @csrf
+                                                            <input name="item_id" type="hidden"
+                                                                value="{{ $product->item_id }}">
+                                                            <input name="sku_id" type="hidden"
+                                                                value="{{ $product->skus[0]->SkuId }}">
+                                                            <button type="submit">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="30"
+                                                                    height="30" fill="currentColor"
+                                                                    class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                                                    <path
+                                                                        d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+                                                                </svg>
+                                                            </button>
+                                                        </form>
                                                     </td>
                                                 </tr>
                                             @endforeach
