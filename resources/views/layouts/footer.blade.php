32115e332dd410de72a1d452628faf6de9490a51
rohmatsyam <rohmatsyam@yahoo.com>
2022-06-10 16:17:42 +0700

    "refactoring footer"

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,233 +1,248 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/footer.css">
     <script src="https://kit.fontawesome.com/ff1fe4e774.js" crossorigin="anonymous"></script>
     <style>
-        .site-footer
-            {
-            background-color:#26272b;
-            padding:45px 0 20px;
-            font-size:15px;
-            line-height:24px;
-            color:#737373;
+        .site-footer {
+            background-color: #26272b;
+            padding: 45px 0 20px;
+            font-size: 15px;
+            line-height: 24px;
+            color: #737373;
+        }
+
+        .site-footer hr {
+            border-top-color: #bbb;
+            opacity: 0.5
+        }
+
+        .site-footer hr.small {
+            margin: 20px 0
+        }
+
+        .site-footer h6 {
+            color: #fff;
+            font-size: 16px;
+            text-transform: uppercase;
+            margin-top: 5px;
+            letter-spacing: 2px
+        }
+
+        .site-footer a {
+            color: #737373;
+        }
+
+        .site-footer a:hover {
+            color: #3366cc;
+            text-decoration: none;
+        }
+
+        .footer-links {
+            padding-left: 0;
+            list-style: none
+        }
+
+        .footer-links li {
+            display: block
+        }
+
+        .footer-links a {
+            color: #737373
+        }
+
+        .footer-links a:active,
+        .footer-links a:focus,
+        .footer-links a:hover {
+            color: #3366cc;
+            text-decoration: none;
+        }
+
+        .footer-links.inline li {
+            display: inline-block
+        }
+
+        .site-footer .social-icons {
+            text-align: right
+        }
+
+        .site-footer .social-icons a {
+            width: 40px;
+            height: 40px;
+            line-height: 40px;
+            margin-left: 6px;
+            margin-right: 0;
+            border-radius: 100%;
+            background-color: #33353d
+        }
+
+        .copyright-text {
+            margin: 0
+        }
+
+        @media (max-width:991px) {
+            .site-footer [class^=col-] {
+                margin-bottom: 30px
             }
-            .site-footer hr
-            {
-            border-top-color:#bbb;
-            opacity:0.5
+        }
+
+        @media (max-width:767px) {
+            .site-footer {
+                padding-bottom: 0
             }
-            .site-footer hr.small
-            {
-            margin:20px 0
+
+            .site-footer .copyright-text,
+            .site-footer .social-icons {
+                text-align: center
             }
-            .site-footer h6
-            {
-            color:#fff;
-            font-size:16px;
-            text-transform:uppercase;
-            margin-top:5px;
-            letter-spacing:2px
+        }
+
+        .social-icons {
+            padding-left: 0;
+            margin-bottom: 0;
+            list-style: none
+        }
+
+        .social-icons li {
+            display: inline-block;
+            margin-bottom: 4px
+        }
+
+        .social-icons li.title {
+            margin-right: 15px;
+            text-transform: uppercase;
+            color: #96a2b2;
+            font-weight: 700;
+            font-size: 13px
+        }
+
+        .social-icons a {
+            background-color: #eceeef;
+            color: #818a91;
+            font-size: 16px;
+            display: inline-block;
+            line-height: 44px;
+            width: 44px;
+            height: 44px;
+            text-align: center;
+            margin-right: 8px;
+            border-radius: 100%;
+            -webkit-transition: all .2s linear;
+            -o-transition: all .2s linear;
+            transition: all .2s linear
+        }
+
+        .social-icons a:active,
+        .social-icons a:focus,
+        .social-icons a:hover {
+            color: #fff;
+            background-color: #29aafe
+        }
+
+        .social-icons.size-sm a {
+            line-height: 34px;
+            height: 34px;
+            width: 34px;
+            font-size: 14px
+        }
+
+        .social-icons a.facebook:hover {
+            background-color: #3b5998
+        }
+
+        .social-icons a.twitter:hover {
+            background-color: #00aced
+        }
+
+        .social-icons a.linkedin:hover {
+            background-color: #007bb6
+        }
+
+        .social-icons a.dribbble:hover {
+            background-color: #ea4c89
+        }
+
+        @media (max-width:767px) {
+            .social-icons li.title {
+                display: block;
+                margin-right: 0;
+                font-weight: 600
             }
-            .site-footer a
-            {
-            color:#737373;
-            }
-            .site-footer a:hover
-            {
-            color:#3366cc;
-            text-decoration:none;
-            }
-            .footer-links
-            {
-            padding-left:0;
-            list-style:none
-            }
-            .footer-links li
-            {
-            display:block
-            }
-            .footer-links a
-            {
-            color:#737373
-            }
-            .footer-links a:active,.footer-links a:focus,.footer-links a:hover
-            {
-            color:#3366cc;
-            text-decoration:none;
-            }
-            .footer-links.inline li
-            {
-            display:inline-block
-            }
-            .site-footer .social-icons
-            {
-            text-align:right
-            }
-            .site-footer .social-icons a
-            {
-            width:40px;
-            height:40px;
-            line-height:40px;
-            margin-left:6px;
-            margin-right:0;
-            border-radius:100%;
-            background-color:#33353d
-            }
-            .copyright-text
-            {
-            margin:0
-            }
-            @media (max-width:991px)
-            {
-            .site-footer [class^=col-]
-            {
-                margin-bottom:30px
-            }
-            }
-            @media (max-width:767px)
-            {
-            .site-footer
-            {
-                padding-bottom:0
-            }
-            .site-footer .copyright-text,.site-footer .social-icons
-            {
-                text-align:center
-            }
-            }
-            .social-icons
-            {
-            padding-left:0;
-            margin-bottom:0;
-            list-style:none
-            }
-            .social-icons li
-            {
-            display:inline-block;
-            margin-bottom:4px
-            }
-            .social-icons li.title
-            {
-            margin-right:15px;
-            text-transform:uppercase;
-            color:#96a2b2;
-            font-weight:700;
-            font-size:13px
-            }
-            .social-icons a{
-            background-color:#eceeef;
-            color:#818a91;
-            font-size:16px;
-            display:inline-block;
-            line-height:44px;
-            width:44px;
-            height:44px;
-            text-align:center;
-            margin-right:8px;
-            border-radius:100%;
-            -webkit-transition:all .2s linear;
-            -o-transition:all .2s linear;
-            transition:all .2s linear
-            }
-            .social-icons a:active,.social-icons a:focus,.social-icons a:hover
-            {
-            color:#fff;
-            background-color:#29aafe
-            }
-            .social-icons.size-sm a
-            {
-            line-height:34px;
-            height:34px;
-            width:34px;
-            font-size:14px
-            }
-            .social-icons a.facebook:hover
-            {
-            background-color:#3b5998
-            }
-            .social-icons a.twitter:hover
-            {
-            background-color:#00aced
-            }
-            .social-icons a.linkedin:hover
-            {
-            background-color:#007bb6
-            }
-            .social-icons a.dribbble:hover
-            {
-            background-color:#ea4c89
-            }
-            @media (max-width:767px)
-            {
-            .social-icons li.title
-            {
-                display:block;
-                margin-right:0;
-                font-weight:600
-            }
-            }
+        }
     </style>
     <title>footer</title>
 </head>
+
 <body>
-    
 
-<!-- Site footer -->
-  <footer class="site-footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12 col-md-6">
-          <h6>Digital Multisite </h6>
-          <p class="text-justify">Digital Multisite adalah layanan dengan menggunakan berbagai channel yang dijadikan strategi perusahaan untuk meningkatkan kepuasan pelanggan, penjualan perusahaan, dan kebutuhan perusahaan lainnya. Digital Multisite adalah metode integratif antara hubungan pelanggan dengan perusahaan.</p>
-        </div>
 
-        <div class="col-xs-6 col-md-3">
-          <h6>Features</h6>
-          <ul class="footer-links">
-            <li><a href="#"style="text-decoration: none;">Katalog</a></li>
-            <li><a href="#"style="text-decoration: none;">Pesanan</a></li>
-            <li><a href="#"style="text-decoration: none;">Persediaan</a></li>
-            <li><a href="#"style="text-decoration: none;">Manajemen Gudang</a></li>
-            <li><a href="#"style="text-decoration: none;">Akuntansi</a></li>
-          </ul>
-        </div>
+    <!-- Site footer -->
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <h6>Digital Multisite </h6>
+                    <p class="text-justify">Digital Multisite adalah layanan dengan menggunakan berbagai channel yang
+                        dijadikan strategi perusahaan untuk meningkatkan kepuasan pelanggan, penjualan perusahaan, dan
+                        kebutuhan perusahaan lainnya. Digital Multisite adalah metode integratif antara hubungan
+                        pelanggan dengan perusahaan.</p>
+                </div>
 
-        <div class="col-xs-6 col-md-3">
-          <h6>Contact Us</h6>
-          <ul class="footer-links">
-            <li><i class="fa-solid fa-phone">&nbsp;&nbsp;</i>061 - 6628728</li>
-            <li><i class="fa-solid fa-envelope">&nbsp;&nbsp;</i>zuhairsyahf@gmail.com</li>
-            <li><i class="fa-solid fa-location-dot">&nbsp;&nbsp;</i>Jalan Kupie Atjeh No. 20, Medan</li>
-          </ul>
-        </div>
-      </div>
-         <h6 style="color: #fff;">FIND US ON MAPS</h6>
-		    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d254832.60117458165!2d98.52940633788393!3d3.6422756426297282!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x303131cc1c3eb2fd%3A0x23d431c8a6908262!2sMedan%2C%20Kota%20Medan%2C%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1653454875759!5m2!1sid!2sid" style="width: 100%; height: 195px;" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-      <hr>
-    </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 col-sm-6 col-xs-12">
-          <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by PT Digital Multisite Indonesia.
-          </p>
-        </div>
+                <div class="col-xs-6 col-md-3">
+                    <h6>Features</h6>
+                    <ul class="footer-links">
+                        <li><a href="#" style="text-decoration: none;">Katalog</a></li>
+                        <li><a href="#" style="text-decoration: none;">Pesanan</a></li>
+                        <li><a href="#" style="text-decoration: none;">Persediaan</a></li>
+                        <li><a href="#" style="text-decoration: none;">Manajemen Gudang</a></li>
+                        <li><a href="#" style="text-decoration: none;">Akuntansi</a></li>
+                    </ul>
+                </div>
 
-        <div class="col-md-4 col-sm-6 col-xs-12">
-          <ul class="social-icons">
-            <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-            <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-            <li><a class="instagram" href="#"><i class="fa fa-instagram"></i></a></li>
-            <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
-          </ul>
+                <div class="col-xs-6 col-md-3">
+                    <h6>Contact Us</h6>
+                    <ul class="footer-links">
+                        <li><i class="fa-solid fa-phone">&nbsp;&nbsp;</i>+6281-123-321-xxx</li>
+                        <li><i class="fa-solid fa-envelope">&nbsp;&nbsp;</i>dms@gmail.com</li>
+                        <li><i class="fa-solid fa-location-dot">&nbsp;&nbsp;</i>DKI Jakarta</li>
+                    </ul>
+                </div>
+            </div>
+            <h6 style="color: #fff;">FIND US ON MAPS</h6>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d253840.65295200603!2d106.6894286393006!3d-6.229386696738052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f3e945e34b9d%3A0x5371bf0fdad786a2!2sJakarta%2C%20Daerah%20Khusus%20Ibukota%20Jakarta!5e0!3m2!1sid!2sid!4v1654852452391!5m2!1sid!2sid"
+                style="width: 100%; height: 195px;" frameborder="0" style="border:0;" allowfullscreen=""
+                aria-hidden="false" tabindex="0"></iframe>
+            <hr>
         </div>
-      </div>
-    </div>
-</footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-sm-6 col-xs-12">
+                    <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by PT Digital Multisite
+                        Indonesia.
+                    </p>
+                </div>
 
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <ul class="social-icons">
+                        <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a class="instagram" href="#"><i class="fa fa-instagram"></i></a></li>
+                        <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
